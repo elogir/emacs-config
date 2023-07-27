eda9d254c8bd50efd4b5ffd2fdd32e29c3075b46
rigole <raphael.nahoum@epita.fr>
2023-07-28 00:14:49 +0200

    Theme and custom.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "GOOG" :family "RobotoMono Nerd Font"))))
+ '(highlight ((t (:background "spring green" :foreground "#4c4f69"))))
+ '(match ((t (:background "light salmon" :foreground "#e6e9ef")))))
