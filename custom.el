ff7dfdf7ac851ab3f356cb8e1a24e6f8eca17069
rigole <rigole@duck.com>
2023-07-30 22:23:33 +0200

    no more dimmer

--- custom.el
+++ custom.el
@@ -9,6 +9,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "GOOG" :family "RobotoMono Nerd Font"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "outline" :family "RobotoMono Nerd Font"))))
  '(highlight ((t (:background "spring green" :foreground "#4c4f69"))))
  '(match ((t (:background "light salmon" :foreground "#e6e9ef")))))
